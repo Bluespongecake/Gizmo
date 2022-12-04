c55dffc4a884a4bd8cf85000c5a4121efb0cb522
Max McCormack <47184910+Bluespongecake@users.noreply.github.com>
2022-12-04 14:00:23 +0000

    Added thorough comments

--- PL_AI.ino
+++ PL_AI.ino
@@ -1,6 +1,6 @@
 //Libraries to include
 #include <Servo.h>
-#include <Wire.h>
+#include <Wire.h>                          // I2C libraries
 #include <hd44780.h>                       // main hd44780 header
 #include <hd44780ioClass/hd44780_I2Cexp.h> // i2c expander i/o class header
 
@@ -26,16 +26,27 @@
 #define Z_MIN_PIN          18
 #define Z_MAX_PIN          19
 
-//Store current co-ordinates 
+//Ultrasonic (HC-SR04) pin declerations
+#define echoPin1 16
+#define trigPin1 17
+#define echoPin2 23
+#define trigPin2 25
+#define echoPin3 27
+#define trigPin3 29
+
+//Store current co-ordinates as global variables
 int current_x;
 int current_y;
+
+//Duration of sound wave travel 
+//1 -> Top 
+//3 -> Bottom 
 
 //Store steps to be made 
 int x_steps_to_be_made;
 int y_steps_to_be_made;
 int * steps_to_be_made;
 
-//Pin declerations
 
 //Initialising objects
 Servo penServo;
@@ -43,7 +54,8 @@ hd44780_I2Cexp lcd;
 
 void setup() {
   Serial.begin(9600);
-  
+
+  // set pinmodes for steppers
   pinMode(X_STEP_PIN, OUTPUT);
   pinMode(X_DIR_PIN, OUTPUT);
   pinMode(X_ENABLE_PIN, OUTPUT);
@@ -71,6 +83,7 @@ void setup() {
   current_x = 325;
   current_y = 185;
 
+  //TEST CODE - SINGLE RUN 
   move_pen(0,100); 
   draw(0,200);
   draw(150,0);
@@ -81,17 +94,21 @@ void loop() {
 
 }
 
+//method to lift pen from board
 void pen_up() {
   delay(200);
   penServo.write(0);
 }
 
+// method to contact pen with board
 void pen_down() {
   delay(200);
   penServo.write(90);
 }
 
+// method to draw a line between two cartesian coordinates
 void draw(int change_x, int change_y){
+  lcd_drawing_message();
   pen_down();
   steps_to_be_made = turns_to_dist(current_x, current_y, change_x, change_y, 40, 625, 3200);
   x_steps_to_be_made = steps_to_be_made[1];
@@ -100,9 +117,12 @@ void draw(int change_x, int change_y){
   current_x = current_x + change_x;
   current_y = current_y - change_y;
   pen_up();
+  lcd_start_message();
 }
 
+// method to move pen from one cartesian coordinate to another
 void move_pen(int change_x, int change_y){
+  lcd_drawing_message();
   pen_up(); 
   steps_to_be_made = turns_to_dist(current_x, current_y, change_x, change_y, 40, 625, 3200);
   x_steps_to_be_made = steps_to_be_made[1];
@@ -110,6 +130,7 @@ void move_pen(int change_x, int change_y){
   stepper_speed_ratio(x_steps_to_be_made, y_steps_to_be_made, 40);
   current_x = current_x + change_x;
   current_y = current_y - change_y;
+  lcd_start_message(); 
 }
 
 void draw_TTC_grid() {
@@ -119,6 +140,7 @@ void draw_TTC_grid() {
   move_pen(0,-100);
 }
 
+// test method
 void line_down_center() {
   digitalWrite(X_DIR_PIN, HIGH);
   digitalWrite(Y_DIR_PIN, HIGH);
@@ -132,6 +154,7 @@ void line_down_center() {
   }
 }
 
+//run L motor 1 step
 void single_step_x(){
     digitalWrite(X_STEP_PIN, HIGH);
     delayMicroseconds(300);
@@ -139,6 +162,7 @@ void single_step_x(){
     delayMicroseconds(300);
 }
 
+// run R motor 1 step
 void single_step_y(){
     digitalWrite(Y_STEP_PIN, HIGH);
     delayMicroseconds(300);
@@ -146,6 +170,7 @@ void single_step_y(){
     delayMicroseconds(300);
 }
 
+//wiper motor
 void single_step_z_cw(){
     digitalWrite(Z_STEP_PIN, HIGH);
     delayMicroseconds(400);
@@ -162,6 +187,7 @@ void single_wipe() {
   }
 }
 
+// display scrolling message on LCD
 void lcd_start_message_scrolling() {
   int count = 0;
   lcd.clear();
@@ -178,47 +204,60 @@ void lcd_start_message_scrolling() {
 }
 
 void lcd_start_message() {
-  int count = 0;
   lcd.clear();
   lcd.setCursor(6,0);
   lcd.print("PL.AI");
-  lcd.setCursor(2,1);
+  lcd.setCursor(3  ,1);
   lcd.print("Tic-Tac-Toe");
 }
 
+// display "DRAWING" on LCD
+void lcd_drawing_message() {
+  lcd.clear();
+  lcd.setCursor(4,0);
+  lcd.print("DRAWING");
+}
+
+// self explanatory
 void lcd_display_winner() {
 
 }
 
+// start the game
 void gesture_start (){
   
 }
 
+//end the game
 void gesture_end (){
   
 }
 
-//converts xy distance change to change in motor angles
-//for changes in motor distances the two motors should move at different speeds but complete the movement in the same period of time
-//returns number of steps required for each motor
-// positive for left (X) == clockwise
-// positive for right (Y) = anticlockwise
+// converts xy distance change to change in motor angles
+// for changes in motor distances the two motors should move at different speeds but complete the movement in the same period of time
+// returns number of steps required for each motor
+// positive (extension) for left (X) == clockwise
+// positive (extension) for right (Y) = anticlockwise
 int * turns_to_dist(int start_x, int start_y, int x_dist_delta, int y_dist_delta, int pulley_diameter, int motor_gap, int steps_per_rot){
     static int rotations[2];
-    //left motor
+    // starting lengths for belts
     double start_belt_left = sqrt(pow(start_x, 2) + pow(start_y, 2));
     double start_belt_right = sqrt(pow(motor_gap - start_x, 2) + pow(start_y, 2));
-    double delta_belt_dist_left = sqrt(pow(x_dist_delta + start_x, 2) + pow(y_dist_delta + start_y, 2)) - start_belt_left;
 
+    // changes to belt lengths to be computed
+    double delta_belt_dist_left = sqrt(pow(x_dist_delta + start_x, 2) + pow(y_dist_delta + start_y, 2)) - start_belt_left;
     double delta_belt_dist_right = sqrt(pow(motor_gap - (x_dist_delta + start_x), 2) + pow(y_dist_delta + start_y, 2)) - start_belt_right;
 
+    // steps to rotate each motor (scalar quantities)
     int rot_left = steps_per_rot * (delta_belt_dist_left) / pulley_diameter;
-    //right motor
     int rot_right = steps_per_rot * (delta_belt_dist_right) / pulley_diameter;
+    
     rotations[0] = rot_left;
     Serial.println(rot_left);
     rotations[1] = rot_right;
     Serial.println(rot_right);
+
+    // determine whether to rotate each motor cw or anti cw
     if (rot_left < 0){
           digitalWrite(X_DIR_PIN, LOW);
     } else {
@@ -234,13 +273,18 @@ int * turns_to_dist(int start_x, int start_y, int x_dist_delta, int y_dist_delta
 
 //accepts a number of steps for each motor and smooths the pen movement by running both steppers concurrently. Only accepts positive numbers of steps for each motor
 void stepper_speed_ratio(int x_steps, int y_steps, int steps_per) {
+    
+    //ensure both values are positive
     x_steps = abs(x_steps);
     y_steps = abs(y_steps);
+
+    //determine ratio between steps for each motor
     int larger = (x_steps > y_steps) ? x_steps : y_steps;
     bool x_is_larger = x_steps == larger;
     double ratio = (x_is_larger) ? (double(y_steps) / larger) : (double(x_steps) / larger);
-
     int int_percent_ratio = ratio * steps_per;
+
+    //store number of steps completed for each motor
     int x_completed = 0;
     int y_completed = 0;
 
@@ -248,8 +292,11 @@ void stepper_speed_ratio(int x_steps, int y_steps, int steps_per) {
     // printf("larger: %d\n", larger);
     // printf("x_is_larger: %d\n", x_is_larger);
 
+    // determine how many steps to increment each motor per cycle
     int x_increment = (x_is_larger) ? steps_per : int_percent_ratio;
     int y_increment = (x_is_larger) ? int_percent_ratio : steps_per;
+
+    //run cycle
     for(int i = 0; i < larger - steps_per; i += steps_per) {
         //increment x
         for(int j = 0; j < x_increment; j++) {
@@ -263,10 +310,11 @@ void stepper_speed_ratio(int x_steps, int y_steps, int steps_per) {
         }
 
     }
+
+    //adjustment to x and y for any that have not been completed
     int x_adj = x_steps - x_completed;
     int y_adj = y_steps - y_completed;
 
-    //adjustment to x and y for any that have not been completed
     for(int i = 0; i < x_adj; i++) {
         x_completed++;
         single_step_x();
